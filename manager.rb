e20cc8f759857e43b2d5bf30b825cec78d31ca39
guavalines <konstrictor@gmail.com>
2023-05-15 23:39:26 -0700

    added styling

--- manager.rb
+++ manager.rb
@@ -15,7 +15,9 @@ class Manager
       puts "No Entries Found".center(50, "-")
     end
     entries.each_with_index do |item, index|
+      puts (index+1).to_s.center(50, "-")
       puts item
+      puts "".center(50, "-")
     end
   end
 
